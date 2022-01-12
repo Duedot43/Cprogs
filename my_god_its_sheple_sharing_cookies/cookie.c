6088c8b019a701ebf6a6ffec6991ed86aa0b2e94
Duedot43 <jackgendill@icloud.com>
2022-01-11 21:28:00 -0700

    oo cookies!

---
+++ my_god_its_sheple_sharing_cookies/cookie.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+  float people, cookies, x, y;
+  
+    printf("\nPlease Enter number of shepl- uh i mean people\n");
+    scanf("%f",&people);
+    printf("\nPlease Enter number of cookies\n");
+    scanf("%f",&cookies);
+    
+    printf("%.2f\n",people);
+    
+    printf("%.2f\n",cookies);
+    x = cookies/people;
+    y = cookies-(people*x);
+    
+    printf("Cookies per person %.2f\n", x);
+    
+    printf("Cookies left: %.2f\n", y);
+
+  return 0;
+}
