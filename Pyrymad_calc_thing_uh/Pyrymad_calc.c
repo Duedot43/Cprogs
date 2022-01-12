46e37d852a9592439a45c57f1ac6890463ff7466
Duedot43 <jackgendill@icloud.com>
2022-01-11 21:48:45 -0700

    ay i fixed it

--- Pyrymad_calc_thing_uh/Pyrymad_calc.c
+++ Pyrymad_calc_thing_uh/Pyrymad_calc.c
@@ -3,9 +3,12 @@
 
 int main() {
     float V, base_area, height;
-    printf("Please input the base_area and height:\n");
-    scanf("%f%f", base_area, height);
-    V = (1/3) * base_area * height;
+    printf("Please input the base_area:\n");
+    scanf("%f", &base_area);
+    printf("Please input the height:\n");
+    scanf("%f", &height);
+    V = (base_area * height)/3;
     V = round(V);
+    printf("the height of the pyrymad is %.2f and the base_area is %.2f\n", height, V);
     return 0;
 }
